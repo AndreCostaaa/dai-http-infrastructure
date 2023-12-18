dfaac68df25f172cc2788fd54c24ba4b71be1012
andcost <toscmen.com@gmail.com>
2023-12-18 20:37:11 +0100

    main app layout

--- garage-frontend/src/App.tsx
+++ garage-frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "./components/navigation/NavBar";
+import MainPanel from "./components/MainPanel";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Grid
+      templateAreas={{ lg: `"navbar" "mainpanel"` }}
+      h="95vh"
+      templateRows="repeat(1, 1fr)"
+      templateColumns="repeat(5, 1fr)"
+      gap={4}
+    >
+      <GridItem
+        colSpan={1}
+        borderColor={"#7f7f7f"}
+        borderWidth={1}
+        borderRadius={20}
+      >
+        <NavBar />
+      </GridItem>
+
+      <GridItem
+        colSpan={4}
+        borderColor={"#7f7f7f"}
+        borderWidth={1}
+        borderRadius={20}
+      />
+    </Grid>
+  );
 }
 
-export default App
+export default App;
