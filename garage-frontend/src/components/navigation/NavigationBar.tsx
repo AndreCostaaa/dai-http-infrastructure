474906419b4aa4a7a2c3b2f2ec4db665e7943c63
andcost <toscmen.com@gmail.com>
2023-12-19 14:36:44 +0100

    navigation bar

---
+++ garage-frontend/src/components/navigation/NavigationBar.tsx
@@ -0,0 +1,62 @@
+import {
+  Button,
+  Card,
+  CardHeader,
+  Heading,
+  IconButton,
+  VStack,
+  ButtonProps,
+} from "@chakra-ui/react";
+import { GrServices } from "react-icons/gr";
+
+import { FaCar, FaTools } from "react-icons/fa";
+
+import { MdPeople } from "react-icons/md";
+import NavigationBarButton from "./NavigationBarButton";
+interface Props extends ButtonProps {
+  url: string;
+}
+
+const NavigationBar = () => {
+  const buttons = [
+    {
+      url: "/services",
+      text: "Service",
+      icon: GrServices,
+    },
+    {
+      url: "/clients",
+      text: "Clients",
+      icon: MdPeople,
+    },
+    {
+      url: "/cars",
+      text: "Voitures",
+      icon: FaCar,
+    },
+    {
+      url: "/parts",
+      text: "Pièces",
+      icon: FaTools,
+    },
+    {
+      url: "/employees",
+      text: "Employés",
+      icon: MdPeople,
+    },
+  ];
+  return (
+    <VStack width="100%" padding="20px">
+      {buttons.map((btn, i) => (
+        <NavigationBarButton
+          key={i}
+          url={btn.url}
+          text={btn.text}
+          icon={btn.icon}
+        />
+      ))}
+    </VStack>
+  );
+};
+
+export default NavigationBar;
