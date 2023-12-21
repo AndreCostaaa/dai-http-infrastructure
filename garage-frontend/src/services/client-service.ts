a58de83119dfb3e4ae644d7e16ba5b18e1bc2305
andcost <toscmen.com@gmail.com>
2023-12-21 12:00:20 +0100

    typo

--- garage-frontend/src/services/client-service.ts
+++ garage-frontend/src/services/client-service.ts
@@ -3,7 +3,7 @@ import { Person } from "./http-service";
 export interface Client extends Person {
   email: string;
   street: string;
-  street_no: number;
+  streetNo: number;
   npa: number;
   country: string;
 }
