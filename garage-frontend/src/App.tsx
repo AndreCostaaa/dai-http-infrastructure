c022515feb5af80df12b9c501a86b068144504c2
andcost <toscmen.com@gmail.com>
2023-12-21 11:37:48 +0100

    dashboard panel

--- garage-frontend/src/App.tsx
+++ garage-frontend/src/App.tsx
@@ -6,6 +6,7 @@ import ServicePanel from "./components/service/ServicePanel";
 import EmployeePanel from "./components/employee/EmployeePanel";
 import ClientPanel from "./components/client/ClientPanel";
 import CarPartPanel from "./components/car-part/CarPartPanel";
+import DashboardPanel from "./components/dashboard/DashboardPanel";
 function App() {
   return (
     <Grid
@@ -21,6 +22,7 @@ function App() {
 
       <GridItem colSpan={4}>
         <Routes>
+          <Route path="/" element={<DashboardPanel />}></Route>
           <Route path="cars" element={<CarPanel />}></Route>
 
           <Route path="services" element={<ServicePanel />}></Route>
