f5cb91e6b58f365650b44b3c7337a6355b6bc54a
yahia1018 <yanis.ouadahi@gmail.com>
2023-12-30 09:44:11 +0100

    Reorder App.java

--- garage-backend/src/main/java/dai/http/App.java
+++ garage-backend/src/main/java/dai/http/App.java
@@ -1,40 +1,10 @@
 package dai.http;
 
 import io.javalin.*;
+
 public class App {
-    public static void main(String[] args){
-        try(Javalin app = Javalin.create().start(7000)){
-
-            //RoleController
-            RoleController roleController = new RoleController();
-            app.get("/api/roles", roleController::fetchAll);
-            app.get("/api/roles/{roleId}", roleController::fetchOne);
-            app.post("/api/roles/", roleController::save);
-            app.patch("/api/roles/update/", roleController::update);
-            app.delete("/api/roles/{roleId}", roleController::delete);
-
-            //ServiceController
-            ServiceController serviceController = new ServiceController();
-            app.get("/api/services", serviceController::fetchAll);
-            app.get("/api/services/{serviceId}", serviceController::fetchOne);
-            app.get("/api/services/{carId}", serviceController::fetchServiceByCar);
-            app.get("/api/services/{carId}/{stateId}", serviceController::fetchServiceByCarState);
-            app.get("/api/services/{mechanicId}", serviceController::fetchServiceByMechanic);
-            app.get("/api/services/{mechanicId}/{stateId}", serviceController::fetchServiceByMechanicState);
-            app.get("/api/services/{mechanicId}", serviceController::fetchServiceByMechanicOrFree);
-            app.get("/api/services/{stateId}", serviceController::fetchServiceByState);
-            app.post("/api/services/", serviceController::save);
-            app.patch("/api/services/update/", serviceController::update);
-            app.patch("/api/services/incrementState/{serviceId}", serviceController::incrementState);
-            app.delete("/api/services/{serviceId}", serviceController::delete);
-
-            //PersonController
-            PersonController personController = new PersonController();
-            app.get("/api/person", personController::fetchAll);
-            app.get("/api/person/{personId}", personController::fetchOne);
-            app.post("/api/person/", personController::save);
-            app.patch("/api/person/update/", personController::update);
-            app.delete("/api/person/{personId}", personController::delete);
+    public static void main(String[] args) {
+        try (Javalin app = Javalin.create().start(7000)) {
 
             //CarController
             CarController carController = new CarController();
@@ -61,6 +31,37 @@ public class App {
             app.post("/api/clients/", clientController::saveKnowingId);
             app.patch("/api/clients/update/", clientController::update);
             app.delete("/api/clients/{clientId}", clientController::delete);
+
+            //PersonController
+            PersonController personController = new PersonController();
+            app.get("/api/person", personController::fetchAll);
+            app.get("/api/person/{personId}", personController::fetchOne);
+            app.post("/api/person/", personController::save);
+            app.patch("/api/person/update/", personController::update);
+            app.delete("/api/person/{personId}", personController::delete);
+
+            //RoleController
+            RoleController roleController = new RoleController();
+            app.get("/api/roles", roleController::fetchAll);
+            app.get("/api/roles/{roleId}", roleController::fetchOne);
+            app.post("/api/roles/", roleController::save);
+            app.patch("/api/roles/update/", roleController::update);
+            app.delete("/api/roles/{roleId}", roleController::delete);
+
+            //ServiceController
+            ServiceController serviceController = new ServiceController();
+            app.get("/api/services", serviceController::fetchAll);
+            app.get("/api/services/{serviceId}", serviceController::fetchOne);
+            app.get("/api/services/{carId}", serviceController::fetchServiceByCar);
+            app.get("/api/services/{carId}/{stateId}", serviceController::fetchServiceByCarState);
+            app.get("/api/services/{mechanicId}", serviceController::fetchServiceByMechanic);
+            app.get("/api/services/{mechanicId}/{stateId}", serviceController::fetchServiceByMechanicState);
+            app.get("/api/services/{mechanicId}", serviceController::fetchServiceByMechanicOrFree);
+            app.get("/api/services/{stateId}", serviceController::fetchServiceByState);
+            app.post("/api/services/", serviceController::save);
+            app.patch("/api/services/update/", serviceController::update);
+            app.patch("/api/services/incrementState/{serviceId}", serviceController::incrementState);
+            app.delete("/api/services/{serviceId}", serviceController::delete);
         }
     }
 }
