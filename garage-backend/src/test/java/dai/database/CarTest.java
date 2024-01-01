faac68285d84d87e528576fd6db416b448d72a97
andcost <toscmen.com@gmail.com>
2024-01-01 13:59:43 +0100

    created car test file

---
+++ garage-backend/src/test/java/dai/database/CarTest.java
@@ -0,0 +1,39 @@
+package dai.database;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void fetchOne() {
+    }
+
+    @Test
+    void fetchAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
