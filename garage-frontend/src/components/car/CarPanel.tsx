a35a8a6e4b497d0d6cc4c4e9e9178279cdbf481c
andcost <toscmen.com@gmail.com>
2024-01-08 14:41:04 +0100

    car components

--- garage-frontend/src/components/car/CarPanel.tsx
+++ garage-frontend/src/components/car/CarPanel.tsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Car } from "../../services/car-service";
+import Cars from "./Cars";
+const data: Car[] = [
+  {
+    brand: "wv",
+    chassisNo: "122332323",
+    color: "white",
+    id: 1,
+    model: "golf",
+    ownerId: 2,
+    recType: "232323",
+  },
+];
 const CarPanel = () => {
-  return <div>CarPanel</div>;
+  return <Cars carList={data} />;
 };
 
 export default CarPanel;
