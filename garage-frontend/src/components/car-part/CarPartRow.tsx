4be3496a99e18b6938900ec2c12036bd293b87e8
André Costa <toscmen.com@gmail.com>
2024-01-13 21:57:26 +0100

    assign part to service

--- garage-frontend/src/components/car-part/CarPartRow.tsx
+++ garage-frontend/src/components/car-part/CarPartRow.tsx
@@ -1,9 +1,23 @@
-import { Td, Tr } from "@chakra-ui/react";
+import { Button, Td, Tr, useDisclosure } from "@chakra-ui/react";
 import { CarPart } from "../../services/car-part-service";
+import ServiceModal from "../service/ServiceModal";
+import AssignServiceModal from "../service/AssignServiceModal";
+import SingleServiceModal from "../service/SingleServiceModal";
 interface Props {
   part: CarPart;
 }
 const CarPartRow = ({ part }: Props) => {
+  const {
+    onOpen: onOpenAssign,
+    isOpen: isOpenAssign,
+    onClose: onCloseAssign,
+  } = useDisclosure();
+  const {
+    onOpen: onOpenService,
+    isOpen: isOpenService,
+    onClose: onCloseService,
+  } = useDisclosure();
+
   return (
     <>
       <Tr>
@@ -14,8 +28,30 @@ const CarPartRow = ({ part }: Props) => {
         <Td>{part.supplierRef}</Td>
         <Td>{part.buyPrice}</Td>
         <Td>{part.sellPrice}</Td>
-        <Td>{part.serviceId == null ? "Assigner" : "Details"}</Td>
+        <Td>
+          {part.serviceId == 0 ? (
+            <Button onClick={onOpenAssign}>Assigner</Button>
+          ) : (
+            <Button onClick={onOpenService}>Details</Button>
+          )}
+        </Td>
       </Tr>
+      {isOpenAssign && (
+        <AssignServiceModal
+          isOpen={isOpenAssign}
+          onClose={onCloseAssign}
+          children={null}
+        />
+      )}
+
+      {isOpenService && (
+        <SingleServiceModal
+          isOpen={isOpenService}
+          onClose={onCloseService}
+          children={null}
+          serviceId={part.serviceId}
+        />
+      )}
     </>
   );
 };
