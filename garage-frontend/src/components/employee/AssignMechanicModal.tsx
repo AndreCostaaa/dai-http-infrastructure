702009318a8875fe7355f616760a88630d5fbd1a
André Costa <toscmen.com@gmail.com>
2024-01-13 21:59:30 +0100

    getting data from backend

--- garage-frontend/src/components/employee/AssignMechanicModal.tsx
+++ garage-frontend/src/components/employee/AssignMechanicModal.tsx
@@ -1,8 +1,16 @@
+import useMechanics from "../../hooks/useMechanics";
+import DataSkeleton from "../generic/DataSkeleton";
 import EmployeeModal from "./EmployeeModal";
 import { ModalProps } from "@chakra-ui/react";
 
 const AssignMechanicModal = (props: ModalProps) => {
-  return <EmployeeModal {...props} employeeList={[]} />;
+  const { data, isLoading } = useMechanics();
+
+  return (
+    <DataSkeleton isLoading={isLoading} data={data}>
+      {data && <EmployeeModal {...props} employeeList={data} />}
+    </DataSkeleton>
+  );
 };
 
 export default AssignMechanicModal;
