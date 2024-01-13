5f943ab03f6eca938e00d88992ae7e3a1b517ad0
André Costa <toscmen.com@gmail.com>
2024-01-13 22:01:27 +0100

    adding a possible onClick prop

--- garage-frontend/src/components/employee/Employees.tsx
+++ garage-frontend/src/components/employee/Employees.tsx
@@ -1,32 +1,57 @@
-import { Table, TableContainer, Tbody, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Button,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { Employee } from "../../services/employee-service";
 import EmployeeRow from "./EmployeeRow";
 interface Props {
   employeeList: Employee[];
+  onClick?: (employee: Employee) => void;
 }
 
-const Employees = ({ employeeList }: Props) => {
-  const headers = [
-    "id",
-    "prenom",
-    "nom",
-    "phone",
-    "role id",
-    "specialization id",
-  ];
+const Employees = ({ employeeList, onClick }: Props) => {
+  const headers = (onClick: boolean) => {
+    let headers = [
+      "id",
+      "prenom",
+      "nom",
+      "phone",
+      "role id",
+      "specialization id",
+    ];
+    if (onClick) {
+      headers.push("selectionner");
+    }
+    return headers;
+  };
   return (
     <TableContainer>
       <Table>
         <Thead>
           <Tr>
-            {headers.map((key, i) => (
+            {headers(Boolean(onClick)).map((key, i) => (
               <Th key={i}>{key}</Th>
             ))}
           </Tr>
         </Thead>
         <Tbody>
           {employeeList.map((employee, i) => (
-            <EmployeeRow key={i} employee={employee} />
+            <Tr>
+              <EmployeeRow key={i} employee={employee} />
+              {onClick && (
+                <Td>
+                  <Button onClick={() => onClick(employee)}>
+                    Selectionner
+                  </Button>
+                </Td>
+              )}
+            </Tr>
           ))}
         </Tbody>
       </Table>
