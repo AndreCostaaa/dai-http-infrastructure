86ca023e48649598f20f539676faf01af9c23fb5
Amouti <126121965+Amouti@users.noreply.github.com>
2024-01-15 16:09:20 +0100

    start of the email sender

---
+++ garage-backend/src/main/java/dai/smtp/EmailSender.java
@@ -0,0 +1,89 @@
+/**
+ * Credit : https://www.digitalocean.com/community/tutorials/javamail-example-send-mail-in-java-smtp
+ */
+
+package dai.smtp;
+
+import java.util.Date;
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+
+import java.util.Properties;
+
+import javax.mail.Session;
+
+public class EmailSender {
+
+    final static String password = "mypassword";
+    final static String fromEmail = "myemailid@gmail.com";
+    public static void tlsEmailSend(String toEmail, String subject, String body) {
+        toEmail = "myemail@yahoo.com"; // temp
+
+        System.out.println("TLSEmail Start");
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+        props.put("mail.smtp.port", "587"); //TLS Port
+        props.put("mail.smtp.auth", "true"); //enable authentication
+        props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
+
+        //create Authenticator object to pass in Session.getInstance argument
+        Authenticator auth = new Authenticator() {
+            //override the getPasswordAuthentication method
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(fromEmail, password);
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+
+        sendEmail(session, toEmail,"TLSEmail Testing Subject", "TLSEmail Testing Body");
+
+    }
+
+    private static void sendEmail(Session session, String toEmail, String subject, String body){
+        try
+        {
+            MimeMessage msg = new MimeMessage(session);
+            //set message headers
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress("no_reply@example.com", "NoReply-JD"));
+
+            msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
+
+            msg.setSubject(subject, "UTF-8");
+
+            msg.setText(body, "UTF-8");
+
+            msg.setSentDate(new Date());
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+            System.out.println("Message is ready");
+            Transport.send(msg);
+
+            System.out.println("EMail Sent Successfully!!");
+        }
+        catch (Exception e) {
+            return;
+        }
+    }
+
+}
+
