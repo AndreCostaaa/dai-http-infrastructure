61c2f0dc7c693d44d81bf304e96160c46216920f
André Costa <toscmen.com@gmail.com>
2024-01-15 22:21:17 +0100

    new employee

---
+++ garage-frontend/src/components/employee/NewEmployeeModal.tsx
@@ -0,0 +1,14 @@
+import { ModalProps } from "@chakra-ui/react";
+import React from "react";
+import GenericModal from "../generic/GenericModal";
+import NewEmployee from "./NewEmployee";
+
+const NewEmployeeModal = (props: ModalProps) => {
+  return (
+    <GenericModal {...props}>
+      <NewEmployee />
+    </GenericModal>
+  );
+};
+
+export default NewEmployeeModal;
