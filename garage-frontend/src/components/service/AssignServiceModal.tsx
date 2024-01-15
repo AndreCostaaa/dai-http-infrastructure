cc7728a864e26c44bec1ea7789bb83e779c3ab70
andcost <toscmen.com@gmail.com>
2024-01-15 19:26:30 +0100

    on assign

--- garage-frontend/src/components/service/AssignServiceModal.tsx
+++ garage-frontend/src/components/service/AssignServiceModal.tsx
@@ -2,13 +2,18 @@ import { ModalProps } from "@chakra-ui/react";
 import useServicesByState from "../../hooks/useServicesByState";
 import DataSkeleton from "../generic/DataSkeleton";
 import ServiceModal from "./ServiceModal";
-
-const AssignServiceModal = (props: ModalProps) => {
+import { Service } from "../../services/service-client";
+interface Props extends ModalProps {
+  onAssign?: (service: Service) => void;
+}
+const AssignServiceModal = (props: Props) => {
   const { data, isLoading } = useServicesByState(2);
 
   return (
     <DataSkeleton isLoading={isLoading} data={data}>
-      {data && <ServiceModal {...props} serviceList={data} />}
+      {data && (
+        <ServiceModal {...props} onSelect={props.onAssign} serviceList={data} />
+      )}
     </DataSkeleton>
   );
 };
