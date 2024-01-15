4fe52d508d70ae55cc96723f8496f678e8649c3a
andcost <toscmen.com@gmail.com>
2024-01-15 14:46:58 +0100

    servicedetail modal

---
+++ garage-frontend/src/components/service/ServiceDetailModal.tsx
@@ -0,0 +1,7 @@
+import { ModalProps } from "@chakra-ui/react";
+interface Props extends ModalProps {}
+const ServiceDetailModal = (_: Props) => {
+  return <div>ServiceDetailModal</div>;
+};
+
+export default ServiceDetailModal;
