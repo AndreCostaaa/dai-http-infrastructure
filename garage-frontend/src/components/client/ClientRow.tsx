6cd019768128c5072a70eb0cb868c9297fee04c6
André Costa <toscmen.com@gmail.com>
2024-01-15 21:34:35 +0100

    making client selectable

--- garage-frontend/src/components/client/ClientRow.tsx
+++ garage-frontend/src/components/client/ClientRow.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 const ClientRow = ({ client }: Props) => {
   return (
-    <Tr>
+    <>
       <Td>{client.id}</Td>
       <Td>{client.firstName}</Td>
       <Td>{client.lastName}</Td>
@@ -13,7 +13,7 @@ const ClientRow = ({ client }: Props) => {
       <Td>{client.email}</Td>
       <Td>{client.street + " " + client.streetNo}</Td>
       <Td>{client.npa}</Td>
-    </Tr>
+    </>
   );
 };
 
