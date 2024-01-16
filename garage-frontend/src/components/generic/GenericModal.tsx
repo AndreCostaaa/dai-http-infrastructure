8782e541d4c544116781134bca7952c45d24a599
andcost <toscmen.com@gmail.com>
2024-01-16 15:41:57 +0100

    changed modal size

--- garage-frontend/src/components/generic/GenericModal.tsx
+++ garage-frontend/src/components/generic/GenericModal.tsx
@@ -10,7 +10,7 @@ interface Props extends ModalProps {}
 
 const GenericModal = (props: Props) => {
   return (
-    <Modal {...props} size="6xl">
+    <Modal {...props} size="8xl">
       <ModalContent bg="gray.800">
         <ModalCloseButton />
         <ModalBody>{props.children}</ModalBody>
