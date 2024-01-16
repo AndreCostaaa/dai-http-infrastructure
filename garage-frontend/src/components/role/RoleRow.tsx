4132d835bbbbc8315612bb1d85273ab6e5277a77
andcost <toscmen.com@gmail.com>
2024-01-16 15:42:08 +0100

    removed unused import

--- garage-frontend/src/components/role/RoleRow.tsx
+++ garage-frontend/src/components/role/RoleRow.tsx
@@ -1,4 +1,4 @@
-import { Td, Tr } from "@chakra-ui/react";
+import { Td } from "@chakra-ui/react";
 import { Role } from "../../services/role-service";
 import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
 
