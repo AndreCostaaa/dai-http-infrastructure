7c0ccfb79039936eea1d40c528917fd22f720278
andcost <toscmen.com@gmail.com>
2024-01-16 15:42:22 +0100

    updating service

--- garage-frontend/src/components/service/ServiceDetailModal.tsx
+++ garage-frontend/src/components/service/ServiceDetailModal.tsx
@@ -1,16 +1,41 @@
-import { ModalProps } from "@chakra-ui/react";
+import { ModalProps, useToast } from "@chakra-ui/react";
 import serviceClient, { Service } from "../../services/service-client";
 import GenericModal from "../generic/GenericModal";
 import ServiceDetails from "./ServiceDetails";
 import useServiceById from "../../hooks/useServiceById";
 interface Props extends ModalProps {
   serviceId: number;
+  onSubmit?: (service: Service) => void;
 }
 const ServiceDetailModal = (props: Props) => {
   const { data, setData } = useServiceById(props.serviceId);
-
+  const toast = useToast();
   const onSubmit = (service: Service) => {
-    serviceClient.update(service).then(({ data }) => setData(data));
+    serviceClient
+      .update(service)
+      .then(({ data }) => {
+        setData(data);
+        if (props.onSubmit) {
+          props.onSubmit(data);
+        }
+        toast({
+          title: "Mise à jour effectuée",
+          status: "success",
+          duration: 2000,
+          isClosable: true,
+        });
+      })
+      .catch((error) => {
+        const description =
+          error.response?.data.title || "Connexion Impossible";
+        toast({
+          title: "Erreur",
+          status: "error",
+          description: description,
+          duration: 5000,
+          isClosable: true,
+        });
+      });
   };
   return (
     <GenericModal {...props}>
